b7e7c97c25ad8f2281ccd443c7ec9270b52adea3
Darell Hoei <darellhoei@gmail.com>
2023-05-23 19:50:57 +0700

    added post page

--- src/App.js
+++ src/App.js
@@ -1,85 +1,14 @@
-import { useEffect, useState } from 'react';
-import Canvas from './components/Canvas/Canvas';
-import CanvasContent from './components/CanvasContent/CanvasContent';
+import { Route, Routes } from "react-router-dom";
+import Post from "./pages/Post";
 
 const App = () => {
-    const [title, setTitle] = useState('');
-    const [category, setCategory] = useState('');
-    const [content, setContent] = useState('');
-    const [image, setImage] = useState(null);
-    const [frame, setFrame] = useState(null);
-    const [frame2, setFrame2] = useState(null);
-
-    useEffect(() => {
-        const img = new Image();
-        img.src = require('./components/Canvas/Cover.png');
-        setFrame(img);
-
-        const img2 = new Image();
-        img2.src = require('./components/CanvasContent/Frame2.png');
-        setFrame2(img2);
-    }, []);
-
-    const handleImageUpload = (e) => {
-        var reader = new FileReader();
-        reader.onload = function (event) {
-            const img = new Image();
-            img.src = event.target.result;
-            setImage(img);
-        };
-        reader.readAsDataURL(e.target.files[0]);
-    };
-
-    const handleDownload = (id) => {
-        const link = document.createElement('a');
-        link.download = 'cover.png';
-        link.href = document.getElementById(id).toDataURL();
-        link.click();
-    };
-
-    return (
-        <div className="App">
-            <h1>Instagram Post Generator</h1>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-                <Canvas
-                    contentTitle={title}
-                    contentCategory={category}
-                    contentImage={image}
-                    contentFrame={frame}
-                    width={1080}
-                    height={1080}
-                />
-                <CanvasContent
-                    contentContent={content}
-                    contentCategory={category}
-                    contentFrame={frame2}
-                    width={1080}
-                    height={1080}
-                />
-            </div>
-            <div className="groupInput">
-                <h2>Cover Image</h2>
-                <input className="input" type="file" id="imageLoader" name="imageLoader" onChange={handleImageUpload} />
-            </div>
-            <div className="groupInput">
-                <h2>Title</h2>
-                <textarea className="input" rows="4" value={title} onChange={(e) => setTitle(e.target.value)} />
-            </div>
-            <div className="groupInput">
-                <h2>Category</h2>
-                <input className="input" value={category} onChange={(e) => setCategory(e.target.value)} />
-            </div>
-            <div className="groupInput">
-                <h2>Content</h2>
-                <textarea className="input" rows="18" value={content} onChange={(e) => setContent(e.target.value)} />
-            </div>
-
-            <div className="groupInput">
-                <button onClick={() => handleDownload('canvas-cover')}>Download Cover</button>
-                <button onClick={() => handleDownload('canvas-content')}>Download Content</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/p/:postId" element={<Post />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
